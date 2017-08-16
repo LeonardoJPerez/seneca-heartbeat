835a52f1f738848179312e2ec51c35325856deff
Leonardo Perez <find.leonardo@gmail.com>
2017-08-16 14:33:28 -0400

    Removed github connector.

--- heartbeat-plugin.js
+++ heartbeat-plugin.js
@@ -1,5 +1,3 @@
-const kinesisConnector = require('./lib/connectors/kinesisConnector');
-
 const internal$ = require('./lib');
 
 let seneca$;
@@ -31,35 +29,6 @@ module.exports = function heartbeatPlugin(options = defaults$) {
     seneca$.add({ role: 'heartbeat', cmd: 'serviceName' }, internal$.getServiceName);
     seneca$.add({ role: 'heartbeat', cmd: 'stats' }, internal$.getStats);
     seneca$.add({ role: 'heartbeat', cmd: 'version' }, internal$.getVersion);
-
-    // Piping data to Kinesis.
-    // Test code.
-    seneca$.add({ role: 'heartbeat', cmd: 'send' }, function sendStats(msg, done) {
-        this.act({
-            role: 'heartbeat',
-            cmd: 'get'
-        }, (err, out) => {
-            if (err) {
-                throw err;
-            }
-
-            // Add Kinesis pipe of current stats.
-            console.log('Data sent to kinesis.');
-
-            const currTime = new Date().getMilliseconds();
-            const sensor = `sensor-${Math.floor(Math.random() * 100000)}`;
-            const reading = Math.floor(Math.random() * 1000000);
-
-            const record = {
-                time: currTime,
-                sensor,
-                reading
-            };
-
-            // kinesisConnector.producer.execute(record);
-            done(out);
-        });
-    });
 
     internal$.init.call(seneca$, opts);
 };
